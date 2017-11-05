6819809cb23469b3ccfbe74b26441ffc4b53d339
mohammad.shahjahan <mohammad.shahjahan@jeevansathi.com>
2017-11-05 20:52:07 +0530

    Added counter

--- App.js
+++ App.js
@@ -1,10 +1,60 @@
 import React from 'react';
 
 export default class App extends React.Component {
+	constructor()
+	{
+		super();
+		console.log("i am in constructor.");
+		this.state = {
+			counter : 0
+		}
+	}
+
+	componentWillMount()
+	{
+		console.log("I am in componentWillMount");
+	}
+
+
+	componentDidMount()
+	{
+		console.log("I am in componentDidMount");
+		
+	}
+
+
+	componentWillUpdate()
+	{
+		console.log("I am in componentWillUpdate");
+		
+	}
+
+	componentDidUpdate()
+	{
+		console.log("I am in componentDidUpdate");
+	}
+
+	shouldComponentUpdate()
+	{
+		console.log("I am in componentShouldUpdate");
+		return true;
+	}
+
+
+	add()
+	{
+		console.log("inside counter.");
+		this.setState({
+			counter : this.state.counter + 1			
+		});
+	}
+
 	render() {
 		return (
 			<div>
-				Hello From React First App!!
+				{this.state.counter}
+				<br/>
+				<button type="button" onClick={this.add.bind(this)}>Add</button>	
 			</div>
 		);
 	}
